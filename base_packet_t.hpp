5862bdbdb724f72f053cda07f351fcf8ea028894
Zinjkov.su <zinjkov.su@gmail.com>
2018-04-07 00:54:09 +1000

    change package

--- base_packet_t.hpp
+++ base_packet_t.hpp
@@ -14,42 +14,34 @@ namespace rov_types {
     template <uint8_t packet_id_, uint8_t payload_size_, uint16_t packet_size_>
     class base_packet_t : public serializable {
     public:
-		uint8_t serialize(uint8_t *output) override final  {
+            typedef meta_traits<packet_id_, payload_size_, packet_size_> meta;
+	uint8_t serialize(uint8_t *output) override final  {
             binary_stream bs;
-            Serial.println((int)meta.packet_id);   
-            bs << meta.packet_id;
-            // return 0;
+            Serial.println((int)meta().packet_id);   
+            bs << meta().packet_id;
+        
             data_serialize(bs);
-            bs << crc::calculateCRC(bs.data_ptr(), meta.payload_size);
+
+            bs << crc::calculateCRC(bs.data_ptr(), meta().payload_size);
 			uint8_t size = 0;
             bs.data(output, &size);
 
-			// Serial.print("seri ");
-			// Serial.println((int)binary_stream::swap_endian<uint8_t>(output[0]));
-			return size;
+            return size;
         }
 
         error_code deserialize(uint8_t *input, uint8_t size) {
             binary_stream bs(input, size);
-			// Serial.print("bs size ");
-			// Serial.println(bs.len());
+
             uint8_t packet_id;
 
-            if (size < meta.packet_size){
+            if (size < meta().packet_size){
                 return size_less;
             }
 
             bs >> packet_id;
 
-            // Serial.print("meta id ");
-            // Serial.println((int)meta.packet_id);
 
-            // Serial.print("packet id ");
-            // Serial.println((int)input[0]);
-            // Serial.print("packet id ");
-            // Serial.println((int)binary_stream::swap_endian<uint8_t>(packet_id));
-
-            if (packet_id != meta.packet_id) {
+            if (packet_id != meta().packet_id) {
                 return wrong_id;
             }
 
@@ -58,11 +50,11 @@ namespace rov_types {
             int16_t current_crc = 0;
             bs >> current_crc;
 
-            if (current_crc != crc::calculateCRC(input, meta.payload_size)) {
+            if (current_crc != crc::calculateCRC(input, meta().payload_size)) {
                 return crc_mismatch;
             }
 
-            if (size > meta.packet_size){
+            if (size > meta().packet_size){
                 return success_size_greater;
             }
 
@@ -70,7 +62,7 @@ namespace rov_types {
         }
 
     protected:
-        meta_traits<packet_id_, payload_size_, packet_size_> meta;
+
 
         virtual void data_serialize(binary_stream &bs) = 0;
         virtual void data_deserialize(binary_stream &bs) = 0;
